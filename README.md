860650eb3bdf90907e988c52da3f71ba07761d0d
nabila555 <nabila.yesmin555@gmail.com>
2025-09-07 19:35:35 +0600

    folder and file Structure added

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # medical-chatbot
+
+## step-1 create a conda environment after opening repojetory
+
+````bash
+conda create -n medibot python=3.10 -y
+
+``bash
+conda activate medibot
+
+##step-2 install the requarement
+
+``bash
+pip install -r requirements.txt
---
+++ template.py
@@ -0,0 +1,36 @@
+import os
+import logging
+from pathlib import Path
+
+# Setup logging format and level
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+# Define the list of required files and folders
+list_of_files = [
+    "src/__init__.py",
+    "src/helper.py",
+    "src/prompt.py",
+    ".env",
+    "setup.py",
+    "app.py",
+    "research/trials.ipynb",
+   
+]
+
+# Loop through each file and create it if it doesn't exist
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    # Create directory if it doesn't exist
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for the file: {filename}")
+
+    # Create empty file if it doesn't exist or is empty
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+        logging.info(f"Creating empty file: {filepath}")
+    else:
+        logging.info(f"{filename} already exists")
